f36d2d1b419ec6bec6333fd811b35a7bb0ae6114
Sam Fine <samafine1@gmail.com>
2018-02-09 14:49:47 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let capture;
+let slider3 
 
 function setup() {
   createCanvas(620, 440);
   // specify multiple formats for different browsers
   capture = createCapture(VIDEO);
     capture.size(620, 440);
+   Slider3 = createSlider(0.5,1.5,1,0.1);
   noStroke();
   fill(0);
 }
@@ -12,7 +14,8 @@ function setup() {
 function draw() {
   background(255);
   capture.loadPixels();
-  let stepSize = round(constrain(1, 4, 32));
+let val3 = Slider3.value();
+  let stepSize = round(constrain(1, val3, 32));
   for (let y=0; y<height; y+=stepSize) {
     for (let x=0; x<width; x+=stepSize) {
       let i = y * width + x;
